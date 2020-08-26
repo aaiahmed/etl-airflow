a187cb42ca796e75c0c19064f265f5c246981f20
aahmed <aahmed.ai@outlook.com>
2020-08-26 16:40:45 -0400

    Add initial transformation.

---
+++ transform/transform_ny_taxi.py
@@ -0,0 +1,54 @@
+"""
+Reads the extracted taxi data, transforms rdd.
+"""
+from pyspark.sql.functions import lit, monotonically_increasing_id
+from utils import spark_session
+
+
+def transform_taxi_df():
+    """
+    Transforms data and writes into temp.
+    :return:
+    """
+    spark = spark_session.get_spark_session()
+    green_df = spark.read\
+        .csv("temp/green_*.csv",header=True,sep=",")\
+        .withColumn("taxi_type", lit('1'))\
+        .drop("ehail_fee")\
+        .drop("trip_type")\
+        .withColumnRenamed('lpep_pickup_datetime', 'tpep_pickup_datetime')\
+        .withColumnRenamed('lpep_dropoff_datetime', 'tpep_dropoff_datetime')
+
+    yellow_df = spark.read\
+        .csv("temp/yellow_*.csv",header=True,sep=",")\
+        .withColumn('taxi_type', lit('2'))
+
+    green_df\
+        .union(yellow_df)\
+        .withColumn('id', monotonically_increasing_id())\
+        .withColumnRenamed('VendorID', 'vendor_id')\
+        .withColumnRenamed('tpep_pickup_datetime', 'pickup_datetime')\
+        .withColumnRenamed('tpep_dropoff_datetime', 'dropoff_datetime')\
+        .withColumnRenamed('RatecodeID', 'rate_code_id')\
+        .withColumnRenamed('PULocationID', 'pickup_location_id')\
+        .withColumnRenamed('DOLocationID', 'dropoff_location_id')\
+        .select(["id", "vendor_id", "pickup_datetime", "dropoff_datetime",
+                 "pickup_location_id", "dropoff_location_id", "passenger_count",
+                 "trip_distance", "fare_amount", "extra", "mta_tax", "tip_amount",
+                 "tolls_amount", "improvement_surcharge", "total_amount",
+                 "payment_type", "congestion_surcharge", "taxi_type"]) \
+        .repartition(1)\
+        .write\
+        .csv(path='temp/transformed', sep=',', header=True)
+
+
+def main():
+    """
+    Main function.
+    :return:
+    """
+    transform_taxi_df()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/spark_session.py
@@ -0,0 +1,22 @@
+"""
+Creates a spark session to send jobs.
+"""
+from pyspark.sql import SparkSession
+from utils import config
+
+
+def get_spark_session():
+    """
+    Returns a spark session.
+    :return:
+    """
+    conf = config.get_config()
+    master = conf['spark']['master']
+    app_name = conf['spark']['app']
+    deploy_mode = conf['spark']['deploy_mode']
+
+    return SparkSession.builder \
+        .master(master) \
+        .appName(app_name) \
+        .config("spark.submit.deployMode", deploy_mode) \
+        .getOrCreate()
