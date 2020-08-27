ed5810a67401359203bfc6a44b80fed1293e0b95
aaiahmed <aaia11@mun.ca>
2020-08-27 15:02:38 -0400

    Load transformed data into postgresql.

--- transform/transform_ny_taxi.py
+++ transform/transform_ny_taxi.py
@@ -5,13 +5,14 @@ from pyspark.sql.functions import lit, monotonically_increasing_id
 from utils import spark_session
 
 
-def transform_taxi_df():
+def transform_taxi_data():
     """
     Transforms data and writes into temp.
     :return:
     """
     spark = spark_session.get_spark_session()
-    green_df = spark.read\
+    green_df = spark\
+        .read\
         .csv("temp/green_*.csv",header=True,sep=",")\
         .withColumn("taxi_type", lit('1'))\
         .drop("ehail_fee")\
@@ -19,7 +20,8 @@ def transform_taxi_df():
         .withColumnRenamed('lpep_pickup_datetime', 'tpep_pickup_datetime')\
         .withColumnRenamed('lpep_dropoff_datetime', 'tpep_dropoff_datetime')
 
-    yellow_df = spark.read\
+    yellow_df = spark\
+        .read\
         .csv("temp/yellow_*.csv",header=True,sep=",")\
         .withColumn('taxi_type', lit('2'))
 
@@ -37,8 +39,8 @@ def transform_taxi_df():
                  "trip_distance", "fare_amount", "extra", "mta_tax", "tip_amount",
                  "tolls_amount", "improvement_surcharge", "total_amount",
                  "payment_type", "congestion_surcharge", "taxi_type"]) \
-        .repartition(1)\
-        .write\
+        .write \
+        .mode("overwrite") \
         .csv(path='temp/transformed', sep=',', header=True)
 
 
@@ -47,7 +49,7 @@ def main():
     Main function.
     :return:
     """
-    transform_taxi_df()
+    transform_taxi_data()
 
 
 if __name__ == "__main__":
